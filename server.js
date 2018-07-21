17ca631db12acea33eddd876c07dafd38c26b819
MichaelTd <tsouchlarakis@gmail.com>
2018-07-22 01:05:09 +0300

    mime

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http'), fs = require('fs')
 
 var port = process.env.PORT || 3000
@@ -16,3 +17,58 @@ http.createServer(function(req, res) {
     res.end()
   })
 }).listen(port)
+*/
+
+var http = require("http"),
+  url = require("url"),
+  path = require("path"),
+  fs = require("fs"),
+  port = process.env.PORT || 3000,
+  mimeTypes = {
+      "html": "text/html",
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "png": "image/png",
+      "pdf": "application/pdf",
+      "js": "text/javascript",
+      "css": "text/css"
+    };
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname,
+      filename = path.join(process.cwd(), uri);
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory())
+      filename += '/bio-cv.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var mimeType = mimeTypes[filename.split('.').pop()];
+
+      if (!mimeType) {
+        mimeType = 'text/plain';
+      }
+
+      response.writeHead(200, { "Content-Type": mimeType });
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port));
+
+//console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
